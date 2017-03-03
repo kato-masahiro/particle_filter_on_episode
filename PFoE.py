009ccfaa5fb13680bd39b0fba246213646095b0d
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-03-03 14:52:26 +0900

    decision_makingの実装
    テストをいろいろした
    上手く行ったと思う

--- PFoE.py
+++ PFoE.py
@@ -10,18 +10,19 @@ PFoEのための関数を提供するモジュール
 likelihood_function.pyの中で尤度関数の内容を定義
 """
 class Robot:
-    def __init__(self,sensor,choice,particle=1000,maximum=100,resetting=False):
-        self.sensor = [None for i in range(sensor)] #センサ値
+    def __init__(self,sensor,choice,particle = 1000,maximum = 100,resetting = False,reduction = 0.0):
+        self.sensor = [None for i in range(sensor)] #センサ値が入るリスト
         self.particle_num = particle #パーティクルの数
-        self.particle_distribution = [0 for i in range(particle)] #パーティクルの分布数
+        self.particle_distribution = [0 for i in range(particle)] #パーティクルの分布場所
         self.particle_weight = [1.0 / particle for i in range(particle)] #パーティクルの重み
-        self.choice = choice #選択肢の数
+        self.choice = choice #ロボットが取りうる選択肢の数
         self.action = None #選択された行動
         self.reward = None  #報酬値
-        self.episode = [[self.sensor,self.action,self.reward]] #エピソード集合
-        self.maximum = maximum #エピソード数の上限
-        self.resetting = resetting #resettingを行うかどうか
-        self.alpha = 0.0 #各パーティクルの尤度の平均
+        self.episode = [ [self.sensor,self.action,self.reward] ] #エピソード集合
+        self.maximum = maximum #エピソード集合に保存されるイベント数の上限
+        self.resetting = resetting #resettingを行うか否か
+        self.alpha = 0.0 #各パーティクルの重みの平均
+        self.reduction_rate = reduction #辻褄のあわないエピソードをどの程度削減するか
 
     def add_event(self,sensor,action,reward):
         """
@@ -34,10 +35,13 @@ class Robot:
         l.append(sensor)
         l.append(action)
         l.append(reward)
-        self.episode.append(l)
+        if(self.episode[0][1] is None):
+            self.episode[0] = l
+        else:
+            self.episode.append(l)
+
         if( len(self.episode) > self.maximum ):
             del self.episode[0]
-
 
     def likeli_func(self):
         """
@@ -45,15 +49,6 @@ class Robot:
         """
         l = likelihood_function.func(self.sensor,self.episode)
         print "尤度:",l
-
-    def test(self):
-        """
-        処理: てすと
-        引数: sensor
-        戻り値:sensor*2
-        """
-        for i in range(len(self.sensor)):
-            self.sensor[i] = self.sensor[i] * 2
 
     def sensor_update(self):
         """
@@ -78,6 +73,7 @@ class Robot:
     def particle_resampling(self):
         """
         処理:重みに基づきパーティクルをリサンプリングする
+             1 % のパーティクルはランダムにリサンプリングする
         引数: - 
         戻り値: -
         """
@@ -88,15 +84,19 @@ class Robot:
                 if self.particle_distribution[ii] == i:
                     weight_of_episode[i] += self.particle_weight[ii]
         for i in range(self.particle_num):
-            seed = random.randint(1,10000)
-            seed = float(seed) / 10000
-            for ii in range( len(self.episode) ):
-                seed -= weight_of_episode[ii]
-                if seed <= 0:
-                    self.particle_distribution[i] = ii
-                    break
+            seed = random.randint(1,100)
+            if seed == 1:
+                self.particle_distribution[i] = random.randint(0,len(self.episode) - 1)
+            else:
+                seed = random.randint(1,10000)
+                seed = float(seed) / 10000
+                for ii in range( len(self.episode) ):
+                    seed -= weight_of_episode[ii]
+                    if seed <= 0:
+                        self.particle_distribution[i] = ii
+                        break
 
-    def random_resampling(self):
+    def equality_resampling(self):
         """
         処理:全てのパーティクルをエピソード中に均等にリサンプリングする
              全てのパーティクルの重みを均等にする
@@ -110,24 +110,67 @@ class Robot:
     def decision_making(self):
         """
         処理:パーティクルの分布から行動を決定して返す
-        今はとりあえず1を返す
-        引数:
-        戻り値:
+        引数: - 
+        戻り値: action
         """
-        return 1
+        vote = [0.0 for i in range(self.particle_num) ]
 
-    def particle_sliding():
+        for i in range(self.particle_num):
+            distance = 0
+            non_zero_reward = 0.0
+            for l in range( len(self.episode) - self.particle_distribution[i] ):
+                distance += 1
+                if self.episode[ self.particle_distribution[i] + l ][2] != 0:
+                    non_zero_reward = self.episode[ self.particle_distribution[i] + l ][2]
+                    break
+            if non_zero_reward != 0:
+                vote[i] = non_zero_reward / distance
+            else:
+                vote[i] = 0.0
+
+        got = [0.0 for i in range(self.choice) ] #各選択肢が持つ得票数
+        for p in range(self.particle_num):
+            for g in range( len(got) ):
+                if self.episode[self.particle_distribution[p]][1] == g:
+                    got[g] += vote[p]
+                    break
+
+        #最大の評価を持つ選択肢を返す
+        #最大値を持つ選択肢が複数存在する場合は、その中からランダムに返す
+        mx = max(got)
+        mx_n = 0 #有効な選択肢が幾つあるか
+        mx_list = [0 for i in range(self.choice)] #有効な選択肢はどれか
+        for i in range(self.choice):
+            if got[i] == mx:
+                mx_list[mx_n] = i
+                mx_n += 1
+        seed = random.randint(0,mx_n-1)
+        action = mx_list[seed]
+        return action
+
+    def particle_sliding(self):
         """
         処理:全てのパーティクルの分布をひとつずらす
         引数:
         戻り値:
         """
-        pass
+        for i in range(self.particle_num):
+            if self.particle_distribution[i] != (len(self.episode) - 1):
+                self.particle_distribution[i] += 1
 
-    def particle_printing():
+    def see_distribution(self):
         """
         処理:パーティクルの分布の様子を画面に表示する
         引数: - 
         戻り値: - 
         """
-        pass
+        particle_numbers = [0 for i in range( len(self.episode) ) ]
+        for i in range(self.particle_num):
+            particle_numbers[ self.particle_distribution[i] ] += 1
+
+        print "A\t|R\t|N\t|Distribution",
+
+        for i in range( len(self.episode) ):
+            print "\n",self.episode[i][1],"\t|",self.episode[i][2],"\t|",particle_numbers[i],"\t|",
+            for ii in range(int( float(particle_numbers[i]) / float(self.particle_num) * 50.0 ) ):
+                print "*",
