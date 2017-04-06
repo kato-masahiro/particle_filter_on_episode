1ac9f08181e6630980f574981ffe2970d9da37c6
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-04-06 11:30:04 +0900

    クラス構造を適切に(?)設計出来た

--- PFoE_module/pfoe.py
+++ PFoE_module/pfoe.py
@@ -1,28 +1,60 @@
 #!/bin/env python
 #coding:utf-8
-
 """
-APIとして関数を定義する
+クラスの定義と
+APIとしてユーザが用いる関数の定義
 """
-import class_definition
+class Event
+    def __init__(self):
+        self.sensor = None
+        self.action = None
+        self.reward = None
 
-def creation_robot(sensor, choice, particle = 1000, limit = 100, threshold = 0.0, step = 4, reduction = 0.0):
-    
-    
-def decision_making():
-    """
-    引数:ロボットの獲得したセンサ値(1次元リスト)
-    戻り値:ロボットの選択した行動
-    """
+class Episode:
+    def __init__(self,limit):
+        self.episode = []
+        self.limit = limit
+    def setEvent(self,event):
+        self.episode.append(event)
+    def getEpisode(self):
+        return self.episode
 
-def set_reward():
-    """
-    引数:ロボットが得た報酬(数値)
-    戻り値:-
-    """
+class Particle:
+    def __init__(self,particle):
+        self.particle_num = particle
+        self.particle_distribution = [0] * particle
+        self.particle_weight =  [1.0 / particle] * particle
 
-def see_distribution():
-    """
-    引数:星の数
-    戻り値:分布の様子のテキスト
-    """
+class Robot:
+    def __init__(self,sensor,choice,particle = 1000,limit = 100,threshold = 0.0,step = 4,reduction = 0.0):
+        self.event = Event(sensor)
+        self.episode = Episode(limit)
+        self.particle = Particle(particle)
+
+        self.choice_num = choice                             #ロボットが取りうる選択肢の数
+        self.alpha = 0.0                                     #各パーティクルの重みの平均
+
+        self.resetting_threshold = threshold                 #resettingを行うか否かを決定する閾値
+        self.resetting_step = step                           #何ステップをresettingに用いるか 
+        self.reduction_rate = reduction                      #辻褄のあわないエピソードをどの程度削減するか
+
+    def decision_making():
+        """
+        引数:ロボットの獲得したセンサ値(1次元リスト)
+        戻り値:ロボットの選択した行動
+        """
+        pass
+
+    def set_reward():
+        """
+        引数:ロボットが得た報酬(数値)
+        戻り値:-
+        """
+        pass
+
+    def see_distribution():
+        """
+        引数:星の数
+        戻り値:分布の様子のテキスト
+        """
+        pass
