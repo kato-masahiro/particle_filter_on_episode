d604c1118fe6a55dca310102b44bb253e28561f9
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-04-10 12:26:24 +0900

    pfoeの基本的な形と変数を定義
    これから各関数を呼び出す部分を作成する
    そののち各関数を実装する

--- PFoE_module/pfoe.py
+++ PFoE_module/pfoe.py
@@ -24,6 +24,7 @@ class Particle:
         self.particle_num = particle
         self.particle_distribution = [0] * particle
         self.particle_weight =  [1.0 / particle] * particle
+        self.alpha = 0.0
 
 class Robot:
     def __init__(self,sensor,choice,particle = 1000,limit = 100,threshold = 0.0,step = 4,reduction = 0.0):
@@ -31,12 +32,14 @@ class Robot:
         self.episode = Episode(limit)
         self.particle = Particle(particle)
 
-        self.choice_num = choice                             #ロボットが取りうる選択肢の数
-        self.alpha = 0.0                                     #各パーティクルの重みの平均
+        self.sensor = [None] * sensor            #ロボットの得たセンサ値
+        self.action = None                       #ロボットが選択した行動
+        self.reward = None                       #ロボットの得た報酬値
+        self.choice_num = choice                 #ロボットが取りうる選択肢の数
 
-        self.resetting_threshold = threshold                 #resettingを行うか否かを決定する閾値
-        self.resetting_step = step                           #何ステップをresettingに用いるか 
-        self.reduction_rate = reduction                      #辻褄のあわないエピソードをどの程度削減するか
+        self.resetting_threshold = threshold     #resettingを行うか否かを決定する閾値
+        self.resetting_step = step               #何ステップをresettingに用いるか 
+        self.reduction_rate = reduction          #辻褄のあわないエピソードをどの程度削減するか
 
     def decision_making():
         """
