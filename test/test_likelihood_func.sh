38492d0d47dda353f030ac2b63f7a3cf2993bb4b
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-04-12 15:31:55 +0900

    テスト用スクリプトの実行結果を画面表示させたい

--- test/test_likelihood_func.sh
+++ test/test_likelihood_func.sh
@@ -5,11 +5,14 @@
 
 #センサ値がいくつあるか
 input_num=$(cat $1 | wc -l)
-past=$(cat $1 | tr '\n' ',')
+past=$(cat $1 | tr '\n' ',' | sed -e 's/,$//')
 
 for n in $(seq 1 $input_num)
 do
     sen=$(sed -n "$n"p sensor_val.txt)
-    echo $sen,[ $past ] | python ../PFoE_module/likelihood_function.py
-
+    echo ===
+    echo sensor: "$sen"
+    echo past: "$past"
+    echo likelihood:
+    echo "$sen",[ "$past" ] | python ../PFoE_module/likelihood_function.py
 done
