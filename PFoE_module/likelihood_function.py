2c6e2d126637433cae9e2533c188c332f8db8b1e
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-04-12 13:56:45 +0900

    likelihood_function.pyのテストに必要なもの

--- PFoE_module/likelihood_function.py
+++ PFoE_module/likelihood_function.py
@@ -2,33 +2,35 @@
 
 import math
 
-def func(sensor,episode):
+def func(sensor,past_sensors):
     """
     概要:尤度関数を定義する.
          過去のエピソードで取得されたセンサ値と、現在取得したセンサ値
          を比較し、適切な尤度を返す。
          具体的な関数の内容は使用者が考えて実装する。
 
-    処理:センサ値(1次元リスト)とエピソード集合(多次元リスト)を受け取り、
-         エピソードの数だけの尤度が入ったリストを返す
+    処理:センサ値のリスト(1×センサ数)と過去のセンサ値のリスト(センサ数×エピソード数)を受け取り、
+         尤度のリスト(1×エピソード数)を返す
 
     引数:sensor[],episode[]
 
     戻り値:likelihood[]
     """
-
-    likelihood = [0.0 for i in range( len(episode) )  ]
+    likelihood = [0.0 for i in range( len(past_sensors) )  ]
     n = [0.0 for i in range( len(sensor) ) ]
 
     """
     このループ内で尤度関数の内容を定義せよ
     """
-    for i in range( len(episode) ):
+    for i in range( len(past_sensors) ):
         for ii in range( len(sensor) ):
-            n[ii] += math.fabs( sensor[ii] - episode[i][0][ii] )
+            n[ii] += math.fabs( sensor[ii] - past_sensors[i][ii] )
         likelihood[i] = 0.5 ** sum(n)
 
         n = [0.0 for i in range( len(sensor) ) ]
 
     return likelihood
 
+if __name__ == '__main__':
+    sensor,episode = input()   
+    print func(sensor,episode)
--- test/test_likelihood_func.sh
+++ test/test_likelihood_func.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 #coding:utf-8
 
-input_num=$(cat sensor_val.txt | wc -l)
+#引数にセンサ値の入ったテキストファイルを取る
+
+#センサ値がいくつあるか
+input_num=$(cat $1 | wc -l)
+past=$(cat $1 | tr '\n' ',')
 
 for n in $(seq 1 $input_num)
 do
-    
+    sen=$(sed -n "$n"p sensor_val.txt)
+    echo $sen,[ $past ] | python ../PFoE_module/likelihood_function.py
+
 done
