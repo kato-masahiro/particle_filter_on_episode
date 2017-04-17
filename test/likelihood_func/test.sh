6aaebd630fe7fac1f3d95fec7f4e621f6e72d5cf
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-04-17 12:13:11 +0900

    test.sh 引数にファイルが指定されていないと終了するように変更

---
+++ test/likelihood_func/test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#coding:utf-8
+
+#引数にセンサ値の入ったテキストファイルを取る
+if [ "$#" -eq 0 ];then
+    echo "引数としてセンサ値の入ったテキストファイルを指定してください"
+    exit 1
+fi
+
+#センサ値がいくつあるか
+input_num=$(cat $1 | wc -l)
+past=$(cat $1 | tr '\n' ',' | sed -e 's/,$//') #センサ値の改行をカンマに直し、最後のカンマを削減
+
+for n in $(seq 1 $input_num)
+do
+    sen=$(sed -n "$n"p sensor_val.txt)
+    echo ===
+    echo sensor: "$sen"
+    echo past: "$past"
+    echo likelihood:
+    echo "$sen",[ "$past" ] | python ../../PFoE_module/likelihood_function.py
+done
