06cddb15c1b36528f063766f58b678e3de8e3c80
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-04-18 10:00:12 +0900

    diff.shのディレクトリのパスを変更
    gitのホームディレクトリからのパスにした

--- test/test_likelihood_func/diff.sh
+++ test/test_likelihood_func/diff.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #coding:utf-8
 
-./test.sh sensor_val.txt > result
-diff result ref
+./test/test_likelihood_func/test.sh ./test/test_likelihood_func/sensor_val.txt > result
+diff result ./test/test_likelihood_func/ref
 echo $?
