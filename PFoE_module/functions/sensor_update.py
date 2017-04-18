39f85904714973c5a86a9a33559ebc3f60565179
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-04-18 16:25:20 +0900

    更新

--- PFoE_module/functions/sensor_update.py
+++ PFoE_module/functions/sensor_update.py
@@ -1,15 +1,23 @@
-#coding:utf-8
+#codin:utf-8
 import likelihood_function
 
-def sensor_update(self,sensor_val,event,episode,particle):
+def func(self,sensor_val,episode,particle):
     """
     処理:全てのパーティクルについて、尤度に基づいて重みを更新する
          重みの平均値でalphaを更新する
          alphaを用いて重みを正規化(すべてのパーティクルの重みの和が1)する
-    引数: センサ値、イベントクラス、エピソードクラス、パーティクルクラス
+    引数: センサ値、エピソードクラス、パーティクルクラス
     戻り値: particleクラス
     """
-    likelihood = likelihood_function.func(sensor_val, episode.sets) 
+    # episode.setsの中のセンサ値集合をまとめる
+    sensor_set = []
+    if len(episode.sets) == 0: #エピソード集合がまだない場合
+        return particle
+    else:
+        for i in range(episode.sets):
+            sensor_set.append(episode.sets.[i].sensor)
+
+    likelihood = likelihood_function.func(sensor_val,sensor_set) 
     particle.alpha = 0.0
 
     for i in range(particle.num):
@@ -23,3 +31,6 @@ def sensor_update(self,sensor_val,event,episode,particle):
         for i in range(particle.num):
             self.particle_weight[i] = 1.0 / self.particle_num
     return particle
+
+if __name__=='__main__':
+    pass
