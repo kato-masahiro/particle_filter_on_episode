6ccab33a5c568994a4cd70e686c57ffac7626e2e
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-04-18 11:52:47 +0900

    pfoe.pyの編集
    関数をインポートする場所を変更

--- PFoE_module/pfoe.py
+++ PFoE_module/pfoe.py
@@ -4,8 +4,9 @@
 クラスの定義と
 APIとしてユーザが用いる関数の定義
 """
-
-import functions
+import sys
+sys.path.append("./functions")
+import likelihood_function
 
 class Event:
     def __init__(self):
@@ -18,9 +19,9 @@ class Episode:
         self.sets = []
         self.limit = limit
     def setEvent(self,event):
-        self.episode.append(event)
+        self.sets.append(event)
     def getEpisode(self):
-        return self.episode
+        return self.sets
 
 class Particle:
     def __init__(self,particle):
@@ -47,7 +48,12 @@ class Robot:
     def decision_making(self,sensor_val):
         """
         引数:ロボットの獲得したセンサ値(1次元リスト)
-        戻り値: - 
+        戻り値: self.action(整数) 
+        処理: 
+        1. 受け取ったセンサ値を用いて、各パーティクルの尤度を更新する(sensor_update)
+        2. パーティクルの合計値がthreshold以下の場合には、回想による尤度のリセットを行う(retrospective_resetting)
+        3. 尤度に基づき、パーティクルのリサンプリングを行う(particle_resampling)
+        4. パーティクルの分布に基づき、最善と思われる行動を求める(decision_making)
         """
         self.particle = functions.sensor_update(sensor_val,self.event,self.episode,self.particle)
         """
@@ -60,10 +66,11 @@ class Robot:
         """
         引数:ロボットが得た報酬(数値)
         戻り値:-
-        """
         add_event()
         weight_reduction()
         particle_sliding()
+        """
+        pass
 
     def see_distribution():
         """
