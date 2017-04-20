acf6735f053e345edddf6309a51cdd51bc79ee23
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-04-20 21:44:01 +0900

    functionsの関数 sensor_update および、.test_sensor_updateの更新
    3パターンのテストにより、sensor_updateはほぼ期待通り動いてくれている
    alpha:正規化前の重みの平均値。

--- PFoE_module/.test_sensor_update.py
+++ PFoE_module/.test_sensor_update.py
@@ -7,38 +7,45 @@ import unittest
 from functions import sensor_update
 import class_definition 
 
-robot1 = class_definition.Robot(sensor=4,choice=3,particle=12)
+robot1 = class_definition.Robot(sensor=4,choice=3,particle=12) #尤度が重みにちゃんと掛けられているかチェック
+robot2 = class_definition.Robot(sensor=4,choice=3,particle=20) #エピソード数が0の場合のチェック
+robot3 = class_definition.Robot(sensor=4,choice=3,particle=10) #エピソードの重みがすべて0の場合のチェック
 
 robot1.particle.distribution=[0,1,2,0,1,2,0,1,2,0,1,2]
 robot1.particle.weight = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
 
-print robot1.episode.sets
-print "---"
+robot3.particle.distribution=[0,1,2,0,1,2,0,1,2,0]
+robot3.particle.weight = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 
 robot1.event.sensor = [1,1,1,1]
 robot1.event.action = 1
 robot1.event.reward = 1.0
 robot1.episode.setEvent(robot1.event)
-print robot1.episode.sets[0].sensor
-print "---"
 
 robot1.event.sensor = [0,0,1,1]
 robot1.event.action = 1
 robot1.event.reward = 1.0
 robot1.episode.setEvent(robot1.event)
-print robot1.episode.sets[0].sensor
-print robot1.episode.sets[1].sensor
-print "---"
 
 robot1.event.sensor = [0,0,0,0]
 robot1.event.action = 1
 robot1.event.reward = 1.0
 robot1.episode.setEvent(robot1.event)
-print robot1.episode.sets[0].sensor
-print robot1.episode.sets[1].sensor
-print robot1.episode.sets[2].sensor
-print "---"
+
+robot3.episode = robot1.episode
 
 sen_val = [0,0,0,0]
+
 robot1.particle = sensor_update(sen_val,robot1.episode,robot1.particle)
-print robot1.particle.weight
+print "weight:",robot1.particle.weight
+print "alpha:",robot1.particle.alpha
+print "---"
+
+robot2.particle = sensor_update(sen_val,robot2.episode,robot2.particle)
+print "weight:",robot2.particle.weight
+print "alpha:",robot2.particle.alpha
+print "---"
+
+robot3.particle = sensor_update(sen_val,robot3.episode,robot3.particle)
+print "weight:",robot3.particle.weight
+print "alpha:",robot3.particle.alpha
--- PFoE_module/functions.py
+++ PFoE_module/functions.py
@@ -14,12 +14,8 @@ def sensor_update(sensor_val,episode,particle):
     if len(episode.sets) == 0: #エピソード集合がまだない場合
         return particle
     else:
-        print len(episode.sets), "個のエピソード"
         for i in range(len(episode.sets)):
-            print episode.sets[i].sensor,"を追加しました"
             sensor_set.append(episode.sets[i].sensor)
-
-    print sensor_set
 
     likelihood = likelihood_function.func(sensor_val,sensor_set) 
     particle.alpha = 0.0
@@ -31,7 +27,8 @@ def sensor_update(sensor_val,episode,particle):
     if particle.alpha != 0.0:
         for i in range(particle.num):
             particle.weight[i] = particle.weight[i] / particle.alpha
+        particle.alpha /= particle.num
     else:
         for i in range(particle.num):
-            particle_weight[i] = 1.0 / particle_num
+            particle.weight[i] = 1.0 / particle.num
     return particle
