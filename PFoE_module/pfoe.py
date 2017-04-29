4e036587148a7abc362d9cfa4b70cf68d541fd6b
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-04-30 05:45:43 +0900

    pfoe.pyの編集.クラス定義が重複していたので削除した
    理由: Robotクラスに関する定義はclass_definition.pyからインポートさせるべき
    テスト: まだ

--- PFoE_module/pfoe.py
+++ PFoE_module/pfoe.py
@@ -1,56 +1,41 @@
 #!/bin/env python
 #coding:utf-8
 """
-クラスの定義と
-APIとしてユーザが用いる関数の定義
+APIとしてユーザが用いる関数を提供する
 """
-from likelihood_function import func
+from likelihood_function import likelihood_function
+import class_definition
 
-class Robot:
-    def __init__(self,sensor,choice,particle = 1000,limit = 100,threshold = 0.0,step = 4,reduction = 0.0):
-        self.event = Event()
-        self.episode = Episode(limit)
-        self.particle = Particle(particle)
+def decision_making(self,sensor_val):
+    """
+    引数:ロボットの獲得したセンサ値(1次元リスト)
+    戻り値: self.action(整数) 
+    処理: 
+    1. 受け取ったセンサ値を用いて、各パーティクルの尤度を更新する(sensor_update)
+    2. パーティクルの合計値がthreshold以下の場合には、回想による尤度のリセットを行う(retrospective_resetting)
+    3. 重みに基づき、パーティクルのリサンプリングを行う(particle_resampling)
+    4. パーティクルの分布に基づき、最善と思われる行動を求める(decision_making)
+    """
+    self.particle = functions.sensor_update(sensor_val,self.event,self.episode,self.particle)
+    self.particle = retrospective_resetting(self.particles, self.episodes, self.resetting_threshold, self.resetting_step, sensor_val)
+    self.particle = particle_resampling( self.particles,len(self.episodes.events) )
+    action = decision_making()
 
-        self.sensor = [None] * sensor            #ロボットの得たセンサ値
-        self.action = None                       #ロボットが選択した行動
-        self.reward = None                       #ロボットの得た報酬値
-        self.choice_num = choice                 #ロボットが取りうる選択肢の数
+    return action
 
-        self.resetting_threshold = threshold     #resettingを行うか否かを決定する閾値
-        self.resetting_step = step               #何ステップをresettingに用いるか 
-        self.reduction_rate = reduction          #辻褄のあわないエピソードをどの程度削減するか
+def set_reward():
+    """
+    引数:ロボットが得た報酬(数値)
+    戻り値:-
+    add_event()
+    weight_reduction()
+    particle_sliding()
+    """
+    pass
 
-    def decision_making(self,sensor_val):
-        """
-        引数:ロボットの獲得したセンサ値(1次元リスト)
-        戻り値: self.action(整数) 
-        処理: 
-        1. 受け取ったセンサ値を用いて、各パーティクルの尤度を更新する(sensor_update)
-        2. パーティクルの合計値がthreshold以下の場合には、回想による尤度のリセットを行う(retrospective_resetting)
-        3. 重みに基づき、パーティクルのリサンプリングを行う(particle_resampling)
-        4. パーティクルの分布に基づき、最善と思われる行動を求める(decision_making)
-        """
-        self.particle = functions.sensor_update(sensor_val,self.event,self.episode,self.particle)
-        self.particle = retrospective_resetting(self.particles, self.episodes, self.resetting_threshold, self.resetting_step, sensor_val)
-        self.particle = particle_resampling( self.particles,len(self.episodes.events) )
-        action = decision_making()
-
-        return action
-
-    def set_reward():
-        """
-        引数:ロボットが得た報酬(数値)
-        戻り値:-
-        add_event()
-        weight_reduction()
-        particle_sliding()
-        """
-        pass
-
-    def see_distribution():
-        """
-        引数:星の数
-        戻り値:分布の様子のテキスト
-        """
-        pass
+def see_distribution():
+    """
+    引数:星の数
+    戻り値:分布の様子のテキスト
+    """
+    pass
