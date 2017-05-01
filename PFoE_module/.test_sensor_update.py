bd7c326ce453696ffd69db8291a351b7d6b30673
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-05-02 03:46:01 +0900

    .test_sensor_update.pyの編集
    見直しと細かい編集

--- PFoE_module/.test_sensor_update.py
+++ PFoE_module/.test_sensor_update.py
@@ -1,21 +1,24 @@
 #coding:utf-8
 """
-functionモジュールのsensor_update関数をテストする
-"""
-import unittest
+*****************************************************
+* functionモジュールのsensor_update関数をテストする *
+*****************************************************
 
+sensor_update(sensor_val,episodes,particles):
+
+    処理:全てのパーティクルについて、尤度に基づいて重みを更新する
+         重みの平均値でalphaを更新する
+         alphaを用いて重みを正規化(すべてのパーティクルの重みの和が1)する
+    引数: センサ値、エピソードクラス、パーティクルクラス
+    戻り値: particlesクラス
+"""
 from functions import sensor_update
 import pfoe
 
-robot1 = pfoe.Robot(sensor=4,choice=3,particle_num=12) #尤度が重みにちゃんと掛けられているかチェック
-robot2 = pfoe.Robot(sensor=4,choice=3,particle_num=20) #エピソード数が0の場合のチェック
-robot3 = pfoe.Robot(sensor=4,choice=3,particle_num=10) #エピソードの重みがすべて0の場合のチェック
-
+### パターン1 尤度が重みに対して適切なものになっているかどうかチェック###
+robot1 = pfoe.Robot(sensor=4,choice=3,particle_num=12) 
 robot1.particles.distribution=[0,1,2,0,1,2,0,1,2,0,1,2]
 robot1.particles.weight = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
-
-robot3.particles.distribution=[0,1,2,0,1,2,0,1,2,0]
-robot3.particles.weight = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 
 robot1.event.sensor = [1,1,1,1]
 robot1.event.action = 1
@@ -32,9 +35,18 @@ robot1.event.action = 1
 robot1.event.reward = 1.0
 robot1.episodes.setEvent(robot1.event)
 
+### パターン2 エピソード数が0の時の挙動をチェック ###
+robot2 = pfoe.Robot(sensor=4,choice=3,particle_num=20)
+
+### パターン3 エピソードの重みがすべて0の時のチェック ### 
+robot3 = pfoe.Robot(sensor=4,choice=3,particle_num=10)
+robot3.particles.distribution=[0,1,2,0,1,2,0,1,2,0]
+robot3.particles.weight = [0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
 robot3.episodes = robot1.episodes
 
+
 sen_val = [0,0,0,0]
+
 
 robot1.particles = sensor_update(sen_val,robot1.episodes,robot1.particles)
 print "weight:",robot1.particles.weight
