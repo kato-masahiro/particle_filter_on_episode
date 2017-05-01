8c0e0c076c6fc58609e2ca4db2910c7e91a6d605
kato-masahiro <s1426030hj@s.chibakoudai.jp>
2017-05-02 04:33:29 +0900

    .test_retrospective_resettingの修正

--- PFoE_module/.test_retrospective_resetting.py
+++ PFoE_module/.test_retrospective_resetting.py
@@ -1,18 +1,25 @@
 #coding:utf-8
 """
 functionモジュールのretrospective_resetting関数をテストする
+
+retrospective_resetting(particles,episodes,resetting_threshold,resetting_step,sensor_val):
+    処理：
+        Particles.alphaがRobot.resetting_thresholdより小さく、
+        かつ
+        Robot.Episode.sets[] の数が充分存在している場合
+        回想に基づくリセッティングを行う
+    引数: particlesクラス,episodesクラス,resetting_threshold,resetting_step,sensor_val
+    引数: resetting_threshold
+    戻り値:
 """
-import unittest
-
 from functions import retrospective_resetting
-import class_definition 
+import pfoe
 
-robot1 = class_definition.Robot(sensor=4,choice=3,particle_num=12,step=2,threshold=0.5,reduction=0.5)
-
+### ケース1 ###
+robot1 = pfoe.Robot(sensor=4,choice=3,particle_num=12,step=3,threshold=0.5,reduction=0.5)
 robot1.particles.alpha = 0.1
-
 robot1.particles.distribution=[0,1,2,0,1,2,0,1,2,0,1,2]
-robot1.particles.weight = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
+robot1.particles.weight = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
 
 robot1.event.sensor = [1,1,1,1]
 robot1.event.action = 1
@@ -41,3 +48,15 @@ robot1.particles = retrospective_resetting(robot1.particles,robot1.episodes,robo
 print "distribution:",robot1.particles.distribution
 print "weight:",robot1.particles.weight
 print "sum_of_weight:",sum(robot1.particles.weight)
+
+### ケース2 ###
+robot2 = pfoe.Robot(sensor=4,choice=3,particle_num=12,step=3,threshold=0.5,reduction=0.5)
+robot2.particles.alpha = 0.1
+robot2.particles.distribution=[0,1,2,0,1,2,0,1,2,0,1,2]
+robot2.particles.weight = [1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0,1.0]
+
+robot2.particles = retrospective_resetting(robot2.particles,robot2.episodes,robot2.resetting_threshold,robot2.resetting_step,sen_val)
+
+print "distribution:",robot2.particles.distribution
+print "weight:",robot2.particles.weight
+print "sum_of_weight:",sum(robot2.particles.weight)
